53a83722067741218adbe507fd0efba7f4e727aa
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2020-10-28 09:07:40 +0100

    Maps Added

---
+++ Map/backgroundEmpty.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="backgroundEmpty" tilewidth="70" tileheight="70" tilecount="196" columns="14">
+ <image source="back/Backgrounds/backgroundEmpty.png" trans="ff00ff" width="1024" height="1024"/>
+</tileset>
---
+++ Map/sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="sheet" tilewidth="70" tileheight="70" tilecount="98" columns="14">
+ <image source="C:/Users/Isaac/Downloads/sheet.png" trans="ff00ff" width="980" height="490"/>
+</tileset>
